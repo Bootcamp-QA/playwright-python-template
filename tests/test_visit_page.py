866bc9cc424de5af66f57178d827e58547f95cd4
Bootcamp QA <mybootcampqa@gmail.com>
2024-11-13 13:54:45 +0100

    removed mark

--- tests/test_visit_page.py
+++ tests/test_visit_page.py
@@ -1,6 +1,5 @@
 from playwright.sync_api import Page, expect
 
-@pytest.mark.scenario_name("Singup with empty email")
 def test_visit(page: Page):
     print("Given user visit homepage")
     page.goto("https://bootcampqa.com")
